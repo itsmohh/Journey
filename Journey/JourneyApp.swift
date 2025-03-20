a344e7014e30e70a517880e43b11a9ddbc16e58c
Muhammed <olayinkafacus@gmail.com>
2025-03-20 00:19:01 -0400

    Initial Commit

---
+++ Journey/JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyApp.swift
+//  Journey
+//
+//  Created by Yinka  Facus  on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JourneyTests/JourneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyTests.swift
+//  JourneyTests
+//
+//  Created by Yinka  Facus  on 3/20/25.
+//
+
+import Testing
+@testable import Journey
+
+struct JourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
